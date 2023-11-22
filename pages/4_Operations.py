d601328bc131ab0435a82a1c5bd2fb290cd7ac59
M-6NA <markuss.siksna1@gmail.com>
2023-11-22 20:31:55 +0200

    Nothing added

--- pages/4_Operations.py
+++ pages/4_Operations.py
@@ -22,5 +22,17 @@ st.divider()
 
 st.subheader("Table")
 st.subheader("Graphs of important KPI's")
+st.divider()
 
 # ::::::::::::::::: DATA PLOTS AND TABLES ::::::::::::::::: 
+
+col1, col2, col3 = st.columns(3, gap = "small")
+
+with col1:
+    st.header("Column 1")
+
+with col2:
+    st.header("Column 2")
+
+with col3:
+    st.header("Column 3")
